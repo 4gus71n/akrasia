964fed8331f3d2c792f294a640a8fc1beeeab047
Agustin <mellamo.agustin@gmail.com>
2015-05-03 01:30:58 -0300

    New urls.py

--- akrasia/urls.py
+++ akrasia/urls.py
@@ -1,12 +1,11 @@
-from django.conf.urls import patterns, include, url
-
+from django.conf.urls import include, url
 from django.contrib import admin
-admin.autodiscover()
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.generic import TemplateView
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'akrasia.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+urlpatterns = [
+	url(r'^$', TemplateView.as_view(template_name='index.html'), name="Home"),
+    url(r'^api/', include('akrasia_messages.urls'))
+]
 
-    url(r'^admin/', include(admin.site.urls)),
-)
